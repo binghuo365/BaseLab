ec032ca7ba691b0afecebfdf4f0648256370d784
binghuo365 <binghuo365@hotmail.com>
2015-07-01 17:28:36 +0800

    add Mutex test

---
+++ Windows/MutilThread/Mutex.cpp
@@ -0,0 +1,72 @@
+#include "Mutex.h"
+#include "PublicDef.h"
+#include "time.h"
+#include <iostream>
+
+using namespace BaseLabWin;
+
+std::list<Msg> CMutexTest::MsgList;
+int CMutexTest::messageNum(0);
+
+BaseLabWin::CMutexTest::CMutexTest()
+{
+	mutex = CreateMutex(NULL, FALSE, "mutex");
+}
+
+BaseLabWin::CMutexTest::~CMutexTest()
+{
+
+}
+
+DWORD WINAPI BaseLabWin::CMutexTest::producer(LPVOID pParameter)
+{
+	Msg msgtmp;
+	int i = 0;
+	srand((unsigned)time(0));
+	while (TRUE)
+	{
+		//CCRITICAL_SECTION::getInstance().lock();
+		HANDLE hCounterIn = OpenMutex(MUTEX_ALL_ACCESS, FALSE, "mutex");
+		WaitForSingleObject(hCounterIn, INFINITE);
+		if (MsgList.size() < MaxQueSize)
+		{
+			msgtmp.head = ++messageNum;
+			MsgList.push_back(msgtmp);
+			std::cout << "product message :" << msgtmp.head << std::endl;
+			std::cout << "pool message num£º" << MsgList.size() << std::endl;
+		}
+		//CCRITICAL_SECTION::getInstance().unlock();
+		WaitForSingleObject(hCounterIn, INFINITE);
+		CloseHandle(hCounterIn);
+		Sleep(rand() % 1000);
+	}
+
+}
+
+DWORD WINAPI BaseLabWin::CMutexTest::consumer(LPVOID pParameter)
+{
+	std::list<Msg>::iterator it;
+	Msg msgtmp;
+	srand((unsigned)time(0));
+	while (TRUE)
+	{
+		if (!MsgList.empty())
+		{
+			//CCRITICAL_SECTION::getInstance().lock();
+			HANDLE hCounterIn = OpenMutex(MUTEX_ALL_ACCESS, FALSE, "mutex");
+			WaitForSingleObject(hCounterIn, INFINITE);
+			if (!MsgList.empty())
+			{
+				it = MsgList.begin();
+				std::cout << "consume message :" << it->head << std::endl;
+				std::cout << "pool message num:" << MsgList.size() << std::endl;
+				MsgList.pop_front();
+			}
+			WaitForSingleObject(hCounterIn, INFINITE);
+			CloseHandle(hCounterIn);
+			//CCRITICAL_SECTION::getInstance().unlock();
+		}
+		Sleep(rand() % 1000);
+	}
+}
+
