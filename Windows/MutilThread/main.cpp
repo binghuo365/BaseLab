0aabf6909d50b00ea6c4a96cbc69116156fc5bc2
binghuo365 <binghuo365@gmail.com>
2015-07-01 17:22:26 +0800

    add mutex test

--- Windows/MutilThread/main.cpp
+++ Windows/MutilThread/main.cpp
@@ -6,20 +6,31 @@
 #include <Windows.h>
 #include "PublicDef.h"
 #include "CriticalSection.h"
+#include "Mutex.h"
 
 using namespace BaseLabWin;
 
 void main()
 {
+	CMutexTest mutesTest;
+
 	HANDLE producerThread[100];
 	HANDLE consumeThread[100];
 	for (int i = 0; i < 20; ++i)
 	{
-		producerThread[i] = ::CreateThread(NULL, 0, CCRITICAL_SECTION_TEST::producer, NULL, 0, NULL);
+		//1.利用临界区
+		//producerThread[i] = ::CreateThread(NULL, 0, CCRITICAL_SECTION_TEST::producer, NULL, 0, NULL);
+
+		//2.利用互斥量
+		producerThread[i] = ::CreateThread(NULL, 0, CMutexTest::producer, NULL, 0, NULL);
 	}
-	for (int i = 0; i < 21; ++i)
+	for (int i = 0; i < 20; ++i)
 	{
-		consumeThread[i] = ::CreateThread(NULL, 0, CCRITICAL_SECTION_TEST::consumer, NULL, 0, NULL);
+		//1.利用临界区
+		//consumeThread[i] = ::CreateThread(NULL, 0, CCRITICAL_SECTION_TEST::consumer, NULL, 0, NULL);
+
+		//2.利用互斥量
+		consumeThread[i] = ::CreateThread(NULL, 0, CMutexTest::consumer, NULL, 0, NULL);
 	}
 	::WaitForSingleObject(producerThread[0], INFINITE);
 }
