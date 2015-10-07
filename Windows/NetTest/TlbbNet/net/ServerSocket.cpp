520a753e0e19a53231b3528df9c5d26d3ed574f4
binghuo365 <binghuo365@hotmail.com>
2015-10-07 17:52:19 +0800

    delete some comment

--- Windows/NetTest/TlbbNet/net/ServerSocket.cpp
+++ Windows/NetTest/TlbbNet/net/ServerSocket.cpp
@@ -10,36 +10,6 @@ ServerSocket::ServerSocket ( UINT port , UINT backlog )
 
 		BOOL ret = FALSE ;
 
-#define KEY_MAC 12
-#ifdef __WINDOWS__
-
-//#ifndef GAME_CLIENT
-//	char lpszMac[128] = {0};
-//	char lpszKey[128] = {0};
-//	Ini ini( FILE_SERVER_INFO ) ;
-//	ini.ReadText( "KeyDog", "Key", lpszKey, sizeof(lpszKey) ) ;
-//	GetMacByCmd(lpszMac);
-//	int uret = strncmp(lpszKey, lpszMac,KEY_MAC);
-//	if (0 != uret)
-//	{
-//		while (1)
-//		{
-//			MessageBox(NULL,"ÐòÁÐºÅ´íÎó£¬ÎªÁË²»ÎóÉË¶ÓÓÑ£¬ÌáÊ¾ÄãÒ»ÏÂ£¡","ÑÏÖØ´íÎó!",MB_ICONERROR);//ÕýÊ½Ê±×¢ÊÍµô
-//			//double *d = new double[10000];//ÕýÊ½Ê±´ò¿ª
-//		}
-//		//throw 1 ;//ÕýÊ½Ê±´ò¿ª
-//	}
-//	else
-//	{
-//		;
-//	}
-//#else
-//	//if(!CheckSystem())
-//	//	throw 1;
-//#endif
-
-#endif
-
 	// create socket implementation object
 	m_Socket = new Socket;
 //	Assert( m_Socket ) ;
@@ -53,14 +23,12 @@ ServerSocket::ServerSocket ( UINT port , UINT backlog )
 //	Assert( ret ) ;
 	
 	// reuse address before Bind()
-	// ¹ÙÀÎµå ÇÏ±â Àü¿¡ ÁÖ¼Ò ¸®À¯Áî¸¦ ½Ã½ºÅÛ¿¡ ¾Ë·Á¾ß ÇÑ´Ù.
 	ret = m_Socket->setReuseAddr( ) ;
 	if( ret==FALSE )
 		throw 1 ;
 //	Assert( ret ) ;
 	
 	// bind address to socket
-	// ÀÌ¹Ì port°¡ m_Impl¿¡ ÀúÀåµÇ¾î ÀÖÀ¸¹Ç·Î, ÆÄ¶ó¹ÌÅÍ¾ø´Â Bind()¸¦ È£ÃâÇØµµ µÈ´Ù.
 	ret = m_Socket->bind( port ) ;
 	if( ret==FALSE )
 		throw 1 ;
