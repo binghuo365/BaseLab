acb7569393183f50406eae2509fe0f43dc365cb9
binghuo365 <binghuo365@hotmail.com>
2015-10-07 17:30:20 +0800

    extract from tlbb net model init

---
+++ Windows/NetTest/TlbbNet/net/Packet.cpp
@@ -0,0 +1,20 @@
+#include "Packet.h"
+
+Packet::Packet( )
+{
+__ENTER_FUNCTION_FOXNET
+
+
+
+__LEAVE_FUNCTION_FOXNET
+}
+
+Packet::~Packet( )
+{
+__ENTER_FUNCTION_FOXNET
+
+
+
+__LEAVE_FUNCTION_FOXNET
+}
+
