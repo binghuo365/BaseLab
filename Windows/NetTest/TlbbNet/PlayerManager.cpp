e61955ac7098b7c626c4358ba071c0a1528235b9
binghuo365 <binghuo365@hotmail.com>
2015-10-09 15:35:47 +0800

    add connect player entity manager

---
+++ Windows/NetTest/TlbbNet/PlayerManager.cpp
@@ -0,0 +1,57 @@
+
+#include "PlayerManager.h"
+#include "Player.h"
+
+PlayerManager::PlayerManager( )
+{
+	m_nPlayers = 0 ;
+
+	for( INT i=0; i<MAX_PLAYER; i++ )
+	{
+		m_pPlayers[i] = INVALID_ID ;
+	}
+}
+
+PlayerManager::~PlayerManager( )
+{
+	CleanUp( ) ;
+}
+
+VOID PlayerManager::CleanUp( )
+{
+	m_nPlayers = 0 ;
+	for( INT i=0; i<MAX_PLAYER; i++ )
+	{
+		m_pPlayers[i] = INVALID_ID ;
+	}
+}
+
+BOOL PlayerManager::AddPlayer( Player* pPlayer )
+{
+	if( m_pPlayers[m_nPlayers] == INVALID_ID )
+	{
+		m_pPlayers[m_nPlayers] = pPlayer->PlayerID() ;
+		pPlayer->SetPlayerManagerID( m_nPlayers ) ;
+
+		m_nPlayers ++ ;
+	}
+
+	return TRUE ;
+}
+
+BOOL PlayerManager::AddPlayer( Player* pPlayer,UINT MaxPlayer )
+{
+	if(m_nPlayers<MaxPlayer)
+	{
+		if( m_pPlayers[m_nPlayers] == INVALID_ID )
+		{
+			m_pPlayers[m_nPlayers] = pPlayer->PlayerID() ;
+			pPlayer->SetPlayerManagerID( m_nPlayers ) ;
+
+			m_nPlayers ++ ;
+		}
+		
+		return TRUE;
+	}
+}
+
---
+++ Windows/NetTest/TlbbNet/PlayerManager.h
@@ -0,0 +1,44 @@
+#ifndef __PLAYERMANAGER_H__
+#define __PLAYERMANAGER_H__
+
+#include "net/Type.h"
+#include "net/Socket.h"
+#include "Player.h"
+
+
+//管理一群玩家功能的基本模块,一般为每个场景创建一个PlayerManager
+//并且在一个独立的线程内执行
+class PlayerManager
+{
+public :
+	PlayerManager( ) ;
+	~PlayerManager( ) ;
+
+	//清除当前模块内的数据信息
+	VOID				CleanUp( ) ;
+
+	//增加一个玩家
+	BOOL				AddPlayer( Player* pPlayer ) ;
+	BOOL				AddPlayer( Player* pPlayer,UINT MaxPlayer);
+
+	//删除一个玩家
+	VOID				RemovePlayer( PlayerID_t pid ) ;
+
+	PlayerID_t*			GetPlayers( ){ return m_pPlayers ; } ;
+
+	uint				GetPlayerNumber( ){ return m_nPlayers ; } ;
+	BOOL				HasPlayer( ){ return (BOOL)m_nPlayers ; } ;
+
+	//
+	enum
+	{
+		MAX_PLAYER = 10000,
+	};
+	PlayerID_t	m_pPlayers[MAX_PLAYER];
+	uint		m_nPlayers ;
+};
+
+
+
+
+#endif
